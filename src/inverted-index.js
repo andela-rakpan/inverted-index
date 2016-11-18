a573958ac1272b89c00a8aad1f5fdcc420b17998
Raphael Akpan <raphael.akpan@andela.com>
2016-11-18 12:11:56 +0100

    Add function createIndex

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -2,8 +2,31 @@
 /*This is the Index Class. It has methods for creating
 and searching through indexes from a JSON array object*/
 class Index {
-    constructor() {
-        this._indexMap = {};
-        this._i = 1;
-    }
+  constructor() {
+    this._indexMap = {};
+    this._i = 1;
+  }
+
+  createIndex(filePath){
+    filePath.forEach( content =>{
+      for(var property in content){
+        let regex = /\w+/g;
+        let text = content[property].toLowerCase().match(regex);
+
+        text.forEach( word =>{
+          if(!this._indexMap.hasOwnProperty(word)){
+            this._indexMap[word] = [this._i];
+          }else{
+            let check = this._indexMap[word].find(index => index===this._i);
+            if(!check){
+              this._indexMap[word].push(this._i);
+            }
+          }
+
+        });
+
+      }
+      this._i++;
+    });
+  }
 }
