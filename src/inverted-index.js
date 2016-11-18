38c155573b634a7dff40e29cf959200565e1ef3c
Raphael Akpan <raphael.akpan@andela.com>
2016-11-18 11:50:45 +0100

    Create Class Index with constructor

---
+++ src/inverted-index.js
@@ -0,0 +1,9 @@
+'use strict'
+/*This is the Index Class. It has methods for creating
+and searching through indexes from a JSON array object*/
+class Index {
+    constructor() {
+        this._indexMap = {};
+        this._i = 1;
+    }
+}
