b7a0d6c64634987bfde67ee379fa796e3dfa7c72
Raphael Akpan <raphael.akpan@andela.com>
2016-11-19 22:05:14 +0100

    Configure gulpfile to handle jasmine reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-'use strict'
 
 // Gulp configurations to reload browser when files change
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var jasmine = require('gulp-jasmine-livereload-task');
 
-gulp.task('browserSync', () => {
+gulp.task('browserSync', function() {
     browserSync.init({
-        server: {
-            baseDir: 'src'
-        }
+       proxy: "localhost:3000"
     });
 });
 
-gulp.task('start', ['browserSync'], () => {
-    gulp.watch('src/css/**/*.css', browserSync.reload);
-    gulp.watch('src/*.html', browserSync.reload);
-    gulp.watch('src/js/**/*.js', browserSync.reload);
-    gulp.watch('jasmine/spec/**/*.js', browserSync.reload);
+gulp.task('reloadJasmine', jasmine({
+    files: ['jasmine/spec/*.js']
+}));
+
+gulp.task('start', ['reloadJasmine','browserSync'], function() {
+    gulp.watch('src/public/css/*.css', browserSync.reload);
+    gulp.watch('src/views/*.html', browserSync.reload);
+    gulp.watch('src/**/*.js', browserSync.reload);
 });
