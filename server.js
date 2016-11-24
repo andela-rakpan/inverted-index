e2e6c5d95dd5e76ae456f34461fdf1b5c2c54bc1
Raphael Akpan <raphael.akpan@andela.com>
2016-11-24 20:08:30 +0100

    Expose src folder to serve static files

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 //Dependencies
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
 
 //Routes
 app.get('/', function(req, res){
-    res.send('I am working pretty fine');
+    res.sendFile(__dirname + '/src/views/index.html');
 });
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 //Start server
 app.listen(3000, function(req, res){
