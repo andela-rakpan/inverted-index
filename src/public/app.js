ccd0930aad440ca315d94312df0cf6172826d73e
Raphael Akpan <raphael.akpan@andela.com>
2016-11-30 19:11:38 +0100

    Fix ESlint check

--- src/public/app.js
+++ src/public/app.js
@@ -1,20 +1,21 @@
-'use strict';
-var invertedIndex = new Index();
-var indexApp = angular.module('IndexApp', []);
-var fileContent = {};
+/* eslint-disable no-undef */
 
-//Controller for file
-indexApp.controller('fileController', ['$scope', function($scope) {
+const invertedIndex = new Index();
+const indexApp = angular.module('IndexApp', []);
+let fileContent = {};
+
+// Controller for file
+indexApp.controller('fileController', ['$scope', ($scope) => {
   $scope.create = true;
   $scope.upload = false;
   $scope.hideResult = true;
   $scope.indexPanel = true;
 
-  $scope.index  = 0;
+  $scope.index = 0;
   $scope.terms = [];
   $scope.indexMap = {};
   $scope.fileNum = 0;
-  $scope.files   = [];
+  $scope.files = [];
   $scope.searchTerms = [];
   $scope.searchFiles = [];
   $scope.currentFile = '';
@@ -22,63 +23,73 @@ indexApp.controller('fileController', ['$scope', function($scope) {
   $scope.fileObj = {};
 
 
-  //Upload file
-  $scope.uploadFile = function() {
+  // Upload file
+  $scope.uploadFile = function uploadFile() {
     const file = document.getElementById('file-selector').files[0];
-    if(file.type != 'application/json'){
-      $('#msg').html("File MUST be JSON");
+    if (file.type !== 'application/json') {
+      $('#msg').html('File MUST be JSON');
       return;
     }
 
     if (!file) {
-      $('#msg').html("No file selected");
+      $('#msg').html('No file selected');
     } else {
-        if ($scope.files.indexOf(file.name) !== -1) {
-          $('#msg').html("File ALREADY uploaded");
-          return;
-        };
-        $scope.files.push(file.name);
-        var fileReader = new FileReader();
-        fileReader.readAsText(file);
-        fileReader.onload = function(){
-          var content = fileReader.result;
-          fileContent = JSON.parse(content);
-          $('#msg').html("File Uploaded...");
-        };
-        $scope.create = false;
-        $scope.upload = true;
-    };
+      if ($scope.files.indexOf(file.name) !== -1) {
+        $('#msg').html('File ALREADY uploaded');
+        return;
+      }
+      $scope.files.push(file.name);
+      const fileReader = new FileReader();
+      fileReader.readAsText(file);
+      fileReader.onload = function onload() {
+        const content = fileReader.result;
+        fileContent = JSON.parse(content);
+        $('#msg').html('File Uploaded...');
+      };
+      $scope.create = false;
+      $scope.upload = true;
+    }
   };
-  //Create and display index
-  $scope.createInd = function() {
+
+  const reset = function reset() {
+    $scope.searchTerms = [];
+    $scope.searchResult = {};
+    $scope.searchInput = '';
+    $scope.searchCheck = false;
+  };
+
+  // Create and display index
+  $scope.createInd = function createInd() {
     $scope.indexPanel = false;
 
-    //Get number of documents in each document
-    let numObj = Object.keys(fileContent).length;
+    // Get number of documents in each file
+    const numObj = Object.keys(fileContent).length;
     let i = 1;
-    let temp = [];
-    while(i <= numObj){
+    const temp = [];
+    while (i <= numObj) {
       temp.push(i);
-      i++;
+      i += 1;
     }
 
-    $scope.fileNum++;
+    $scope.fileNum += 1;
     invertedIndex.createIndex(fileContent);
     $scope.indexMap = invertedIndex.getIndex();
     $scope.terms = Object.keys($scope.indexMap[$scope.fileNum]);
+    $scope.terms.sort();
     $scope.index = $scope.fileNum;
     $scope.create = true;
     $scope.upload = false;
+    $scope.hideResult = true;
     $scope.currentFile = $scope.files[$scope.index - 1];
     $scope.searchFiles = [];
     $scope.searchFiles.push($scope.currentFile);
     reset();
 
     $scope.fileObj[$scope.currentFile] = temp;
-    $('#msg').html("");
+    $('#msg').html('');
   };
 
-  $scope.display = function(file) {
+  $scope.display = function display(file) {
     $scope.index = $scope.files.indexOf(file) + 1;
     $scope.terms = Object.keys($scope.indexMap[$scope.index]);
     $scope.currentFile = file;
@@ -88,62 +99,49 @@ indexApp.controller('fileController', ['$scope', function($scope) {
     reset();
   };
 
-  $scope.search = function() {
-    let input = $scope.searchInput;
+  $scope.search = function search() {
+    const input = $scope.searchInput;
     let fileIndex = 0;
 
-    if(!input){ return ;}
+    if (!input) { return; }
 
-    let regex = /\w+/g;
+    const regex = /\w+/g;
     let temp = [];
     $scope.searchInput = input.toLowerCase();
-    //handle duplicate values
+    // handle duplicate values and split input terms into words
     temp = $scope.searchInput.match(regex);
-    temp.forEach(function(value){
-        if(true){
-
-        }
+    temp.forEach((value) => {
+      if ($scope.searchTerms.indexOf(value) === -1) {
+        $scope.searchTerms.push(value);
+      }
     });
-
-    $scope.searchTerms = temp;
-
-    $scope.searchFiles.forEach( function (file) {
+    $scope.searchTerms.sort();
+    $scope.searchFiles.forEach((file) => {
       fileIndex = $scope.files.indexOf(file);
-      fileIndex++;
-      //Split input terms into words
+      fileIndex += 1;
       if (fileIndex) {
-        $scope.searchResult[file] = invertedIndex.searchIndex(fileIndex,input);
-        console.log(Object.keys($scope.searchResult[file]));
+        $scope.searchResult[file] = invertedIndex.searchIndex(fileIndex, input);
         $scope.searchInput = '';
       }
     });
-  }
+  };
 
-  var reset = function () {
-    $scope.searchTerms = [];
-    $scope.searchResult = {};
-    $scope.searchInput = '';
-    $scope.searchCheck = false;
-  }
-
-  $scope.searchAll = function () {
-    let checked = $scope.searchCheck;
-    if(checked){
+  $scope.searchAll = function searchAll() {
+    const checked = $scope.searchCheck;
+    if (checked) {
       $scope.searchFiles = $scope.files;
       $scope.searchTerms = [];
-    }else{
+    } else {
       $scope.searchFiles = [];
       $scope.searchFiles.push($scope.currentFile);
     }
-  }
+  };
 
-  $scope.showPanel = function(){
-    if($scope.searchInput){
+  $scope.showPanel = function showPanel() {
+    if ($scope.searchInput) {
       $scope.hideResult = false;
-    }else{
+    } else {
       $scope.hideResult = true;
     }
-  }
+  };
 }]);
-
-
