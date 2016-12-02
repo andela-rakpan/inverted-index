42ad5c432c303a307ec73c43f58c499753cad52e
Raphael Akpan <raphael.akpan@andela.com>
2016-12-02 10:51:11 +0100

    Refactor travis

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/inverted-index-babel.js',
+      'src/inverted-index.js',
       'jasmine/book1.json',
       'jasmine/spec/*.js'
     ],
